34b0c9aa314dd63d9136fa0a73980fe34238103b
Jane Tasevski <janetasevski@MacBook-Pro.local>
2020-09-25 22:48:25 +0200

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCr7LyjFDajtvfvOBZdU7SVJ7W7UBoTJQc",
+    authDomain: "hairdresser-salon-dba5e.firebaseapp.com",
+    databaseURL: "https://hairdresser-salon-dba5e.firebaseio.com",
+    projectId: "hairdresser-salon-dba5e",
+    storageBucket: "hairdresser-salon-dba5e.appspot.com",
+    messagingSenderId: "771117873913",
+    appId: "1:771117873913:web:33ee6dd1fca324226e4028",
+    measurementId: "G-6W8Y098YWK"
+  }
 };
 
 /*
